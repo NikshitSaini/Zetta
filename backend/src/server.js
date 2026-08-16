6c589b3cce3ffe2203009cb9385ba25bd27430c2
NikshitSaini <nikshithansi@gmail.com>
2026-08-16 23:40:57 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import path from "path";
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+const app = express();
+const __dirname = path.resolve();
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    })
+}
+
+app.listen(port,()=>{console.log(`Server is running on port ${port}`)});
